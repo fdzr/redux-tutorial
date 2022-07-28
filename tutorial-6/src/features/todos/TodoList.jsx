d1d76e3e864ccb137ffa01e460c43ace3925e0ec
fdzr <fzamora.reina@gmail.com>
2022-07-28 12:16:00 -0400

    adding dockerfile to run json-server

--- tutorial-6/src/features/todos/TodoList.jsx
+++ tutorial-6/src/features/todos/TodoList.jsx
@@ -20,18 +20,37 @@ const TodoList = () => {
     }
 
     const newItemSection = 
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="new-todo">Enter a new todo item</label>
-            <div className="new-todo">
+        <form onSubmit={handleSubmit}
+            className="flex justify-between border border-slate-600 h-20 items-center">
+            {/* <label htmlFor="new-todo"
+                    className="text-2xl">Enter a new todo item</label> */}
+            <div className="w-full px-3">
                 <input
                     type="text"
                     id="new-todo"
                     value={newTodo}
                     onChange={(e) => setNewTodo(e.target.value)}
                     placeholder="Enter new todo"
+                    className="border 
+                               border-solid
+                               border-slate-400
+                               rounded
+                               w-full
+                               p-[0.5rem]
+                               rounded-[10px]"
                 />
             </div>
-            <button className="submit">
+            <button 
+                className=" min-w-[50px] 
+                            max-w-[50px] 
+                            border 
+                            border-solid 
+                            pointer 
+                            rounded-[10px]
+                            bg-gray-500
+                            text-white
+                            h-[50px]
+                            mr-2">
                 <FontAwesomeIcon icon={faUpload} />
             </button>
         </form>
@@ -46,8 +65,9 @@ const TodoList = () => {
     }
 
     return (
-        <main>
+        <main className="max-w-[600px] m-auto mt-6">
             {newItemSection}
+            {content}
         </main>
     )
 }
